02b2a951ecd7256b84c21d2302eec638e3dd7305
Nitin <nitin.mukund76@gmail.com>
2017-09-21 09:43:09 +0530

    Removed home controller decorator

--- app/controllers/spree/home_controller_decorator.rb
+++
@@ -1,9 +0,0 @@
-Spree::HomeController.class_eval do
-  def index
-    @searcher = build_searcher(params.merge(include_images: true))
-    @products = @searcher.retrieve_products
-    @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
-    @taxonomies = Spree::Taxonomy.includes(root: :children)
-    @products
-  end
-end
